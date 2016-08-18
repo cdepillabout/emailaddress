808851f07a396b544ade0ccb546ac048880c966f
(cdep)illabout <cdep.illabout@gmail.com>
2016-08-18 15:35:20 +0900

    Add additional module documentation.

--- src/Text/Email/Validate.hs
+++ src/Text/Email/Validate.hs
@@ -1,3 +1,26 @@
+{-|
+Module      : Text.Email.Validate
+Description : Wrapper around email-validate adding additional instances.
+Copyright   : (c) Dennis Gosnell, 2016
+License     : BSD3
+
+This module is a wrapper around
+<https://hackage.haskell.org/package/email-validate-2.2.0/docs/Text-Email-Validate.html
+Text.Email.Validate> from <https://hackage.haskell.org/package/email-validate
+email-validate>.
+
+This module exports 'EmailAddress', a newtype wrapper around
+<https://hackage.haskell.org/package/email-validate/docs/Text-Email-Validate.html#t:EmailAddress
+Text.Email.Validate.EmailAddress>.  Additional instances are defined for our
+new 'EmailAddress', including 'ToJSON' and 'FromJSON'. This is done so that no
+orphan instances need to be used.
+
+If you would like additional instances to be defined, please send a
+<https://github.com/cdepillabout/emailaddress pull request>.  Additional
+instances will be accepted for any typeclass from any package available on
+stackage.
+-}
+
 module Text.Email.Validate
     ( -- * Data Type
       EmailAddress
