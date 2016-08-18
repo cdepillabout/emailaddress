3b7a4417c30579b75029cec26156e0b013612e02
(cdep)illabout <cdep.illabout@gmail.com>
2016-08-18 14:14:26 +0900

    Finished exporting functions from internal file.

--- src/Text/Email/Validate.hs
+++ src/Text/Email/Validate.hs
@@ -1,7 +1,21 @@
 module Text.Email.Validate
     ( -- * Data Type
       EmailAddress
+      -- * Create EmailAddress
+    , emailAddress
+    , validate
+      -- * Check validity
+    , isValid
+      -- * Convert back to ByteString
+    , toByteString
+    , localPart
+    , domainPart
+      -- * Helper functions
+    , canonicalizeEmail
+      -- * Unsafe creation
+    , unsafeEmailAddress
     ) where
 
 import Text.Email.Validate.Internal
-    ( EmailAddress )
+    ( EmailAddress, canonicalizeEmail, domainPart, emailAddress, isValid
+    , localPart, toByteString, validate, unsafeEmailAddress )
--- src/Text/Email/Validate/Internal.hs
+++ src/Text/Email/Validate/Internal.hs
@@ -5,7 +5,13 @@
 module Text.Email.Validate.Internal
     ( EmailAddress(unEmailAddress)
     , EmailValidate.isValid
+    , EmailValidate.canonicalizeEmail
+    , emailAddress
     , validate
+    , unsafeEmailAddress
+    , localPart
+    , domainPart
+    , toByteString
     ) where
 
 import Data.ByteString (ByteString)
@@ -21,3 +27,30 @@ newtype EmailAddress = EmailAddress
 
 validate :: ByteString -> Either String EmailAddress
 validate = fmap EmailAddress . EmailValidate.validate
+
+emailAddress :: ByteString -> Maybe EmailAddress
+emailAddress = fmap EmailAddress . EmailValidate.emailAddress
+
+-- | Unsafely create an 'EmailAddress' from a local part and a domain part.
+-- The first argument is the local part, and the second argument is the domain
+-- part.
+--
+-- For example, in the email address @foo\@gmail.com@, the local part is @foo@
+-- and the domain part is @gmail.com@.
+--
+-- >>> toByteString $ unsafeEmailAddress "foo" "gmail.com"
+-- "foo@gmail.com"
+unsafeEmailAddress
+    :: ByteString    -- ^ Local part
+    -> ByteString    -- ^ Domain part
+    -> EmailAddress
+unsafeEmailAddress = (EmailAddress .) . EmailValidate.unsafeEmailAddress
+
+localPart :: EmailAddress -> ByteString
+localPart = EmailValidate.localPart . unEmailAddress
+
+domainPart :: EmailAddress -> ByteString
+domainPart = EmailValidate.domainPart . unEmailAddress
+
+toByteString :: EmailAddress -> ByteString
+toByteString = EmailValidate.toByteString . unEmailAddress
